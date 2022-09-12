d832b455d8573d83a65f1e9b95e3447ab6707fcc
Martin Pasquier <pasquier.martin2@gmail.com>
2022-09-12 22:14:53 +0200

    remove all custom inputs

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-apt-get update && xargs -a /root/apps-list.txt apt-get install -y openssh-server # Install openssh-server and all pkgs in /root/apps-list.txt
-apt-get update && /root/init-custom-script.sh
+apt-get update && apt-get install -y openssh-server # Install openssh-server
 mkdir /root/.ssh && touch /root/.ssh/authorized_keys
 echo ${YourPubKey} > /root/.ssh/authorized_keys
 echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
