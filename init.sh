44630a537987f29c2f5bb78c7dbabc302b49f269
Martin Pasquier <pasquier.martin2@gmail.com>
2022-09-12 22:23:28 +0200

    change way to install and start ssh

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
-apt-get update && apt-get install -y openssh-server # Install openssh-server
 mkdir /root/.ssh && touch /root/.ssh/authorized_keys
 echo ${YourPubKey} > /root/.ssh/authorized_keys
 echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
-service ssh start
 /usr/sbin/sshd -D
